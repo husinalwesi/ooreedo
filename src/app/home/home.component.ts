50625f6e614a85d639f0b1eac59f5036bbc85623
Hussein Al-wesi <alwesihusin@gmail.com>
2022-11-28 00:37:38 +0300

    dfg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { DataService } from '../data.service';
 import { OwlOptions } from 'ngx-owl-carousel-o';
@@ -12,13 +12,13 @@ import { HttpParams } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   cardListSubscribtion = new Subscription();
-  planslist:any;
+  planslist: any;
   planslistCount: any;
   products: any;
-  initialWidth: any =0;
-  cardId: any =1;
+  initialWidth: any = 0;
+  cardId: any = 1;
 
-  constructor(private _DataService:DataService, private _TranslationService:TranslationService,private cdk: ChangeDetectorRef,) { }
+  constructor(private _DataService: DataService, private _TranslationService: TranslationService, private cdk: ChangeDetectorRef,) { }
 
   ngOnInit(): void {
     this.callPlanlist();
@@ -30,15 +30,15 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
   async callPlanlist() {
     await this._DataService.getData();
-    setTimeout(async()=>{
+    setTimeout(async () => {
       //let params = new HttpParams().set("plan_type",this._DataService.planType).set("lang", this._TranslationService.apiLang());
-      let planlist:any = await this._DataService.getPlanslist(this._TranslationService.apiLang());
+      let planlist: any = await this._DataService.getPlanslist(this._TranslationService.apiLang());
       console.log(planlist.data)
       this.planslist = planlist.data;
       this.planslistCount = this.planslist['product_data'].product_count;
       this.products = this.planslist['product_data'].products;
       this.cdk.detectChanges();
-    },5000)
+    }, 1000)
   }
 
   customOptions: OwlOptions = {
@@ -49,7 +49,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     mouseDrag: false,
     touchDrag: true,
     pullDrag: true,
-    rtl: this._TranslationService.isEn()? false :true,
+    rtl: this._TranslationService.isEn() ? false : true,
     dots: true,
     navSpeed: 700,
     navText: ['', ''],
@@ -69,7 +69,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     },
     nav: true
   }
-  slideleft(){
+  slideleft() {
     this.initialWidth = 80;
   }
 }
